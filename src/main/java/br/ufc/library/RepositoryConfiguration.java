d86404cb17bea2f8ddfca37e6baa6aac2f110043
Jordão <jordao05@hotmail.com>
2017-08-07 17:30:03 -0300

    some changes

---
+++ src/main/java/br/ufc/library/RepositoryConfiguration.java
@@ -0,0 +1,15 @@
+package br.ufc.library;
+
+import br.ufc.library.author.Author;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Author.class);
+    }
+}
--- src/main/java/br/ufc/library/author/AuthorRepository.java
+++ src/main/java/br/ufc/library/author/AuthorRepository.java
@@ -1,10 +1,12 @@
 package br.ufc.library.author;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by jordao on 17/07/17.
  */
+@CrossOrigin(origins = "*")
 public interface AuthorRepository extends CrudRepository<Author, Integer>{
 
 }
