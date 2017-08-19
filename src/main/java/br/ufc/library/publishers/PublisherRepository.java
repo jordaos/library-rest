1e7a8fb1b3bc0d27707d347b0ffd90d65e7faf0c
Jordão <jordao05@hotmail.com>
2017-08-19 11:27:34 -0300

    permit request for all origins

--- src/main/java/br/ufc/library/publishers/PublisherRepository.java
+++ src/main/java/br/ufc/library/publishers/PublisherRepository.java
@@ -1,10 +1,12 @@
 package br.ufc.library.publishers;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by jordao on 18/07/17.
  */
+@CrossOrigin(origins = "*")
 public interface PublisherRepository extends CrudRepository<Publisher, Integer>{
 
 }
--- src/main/java/br/ufc/library/pubs/PubRepository.java
+++ src/main/java/br/ufc/library/pubs/PubRepository.java
@@ -1,10 +1,12 @@
 package br.ufc.library.pubs;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by jordao on 18/07/17.
  */
+@CrossOrigin(origins = "*")
 public interface PubRepository extends CrudRepository<Pub, Integer>{
 
 }
