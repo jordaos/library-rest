26d0ff558de992a9e5d54704b9c0806122d5ea42
Jordão <jordao05@hotmail.com>
2017-08-19 10:21:30 -0300

    expose id for Publishers and Pubs

--- src/main/java/br/ufc/library/RepositoryConfiguration.java
+++ src/main/java/br/ufc/library/RepositoryConfiguration.java
@@ -1,6 +1,8 @@
 package br.ufc.library;
 
 import br.ufc.library.author.Author;
+import br.ufc.library.publishers.Publisher;
+import br.ufc.library.pubs.Pub;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -11,5 +13,7 @@ public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Author.class);
+        config.exposeIdsFor(Publisher.class);
+        config.exposeIdsFor(Pub.class);
     }
 }
